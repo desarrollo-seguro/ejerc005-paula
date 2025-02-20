bc1bb86cea2cf3f3ed0f257804718a5859952ff4
curso00 <nocorreo@nocorreo.com>
2025-02-20 11:34:20 +0100

    Refactoring por limpieza del test y añado testing de listar y leer

--- src/test/java/es/santander/ascender/ejerc005/repository/PersonaRepositoryTest.java
+++ src/test/java/es/santander/ascender/ejerc005/repository/PersonaRepositoryTest.java
@@ -1,67 +1,87 @@
 package es.santander.ascender.ejerc005.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import es.santander.ascender.ejerc005.model.Persona;
 
 @SpringBootTest
 public class PersonaRepositoryTest {
 
+    private static final String NOMBRE_PARA_EL_TEST = "Paula";
     @Autowired
     private PersonaRepository repository;
 
+    private Persona personaCreada;
+
+    @BeforeEach
+    public void setUp() {
+        personaCreada = getPersonaYAlmacena();
+    }
+
+    @Test
+    public void testFindById() {
+                Persona personaEncontrada = repository
+                                                .findById(personaCreada.getId())
+                                                .get();
+
+                assertEquals(NOMBRE_PARA_EL_TEST, personaEncontrada.getNombre()); 
+                // Y el resto de campos
+        }
+
+
+        @Test
+        public void testFind() {
+    
+            List<Persona> personas = repository
+                                            .findAll();
+
+            assertTrue(personas.size() >= 1);
+            }        
+
+    private Persona getPersonaYAlmacena() {
+        Persona persona = new Persona();
+        persona.setNombre(NOMBRE_PARA_EL_TEST);
+        persona.setApellido("Gomez");
+        persona.setProvincia_id(55l);
+        repository.save(persona);
+        return persona;
+    }
+
      @Test
     public void testCreate() {
-                Persona persona = new Persona();
-                persona.setNombre("Paula");
-                persona.setApellido("Gomez");
-                persona.setProvincia_id(55l);
-                
-
-                repository.save(persona);
                 assertTrue(
                                 repository
-                                                .findById(persona.getId())
+                                                .findById(personaCreada.getId())
                                                 .isPresent());
         }
 
         @Test
         public void delete() {
-            Persona persona = new Persona();
-            persona.setNombre("Paula");
-            persona.setApellido("Gomez");
-            persona.setProvincia_id(55l);
-            
-
-            repository.save(persona);
                 assertTrue(
                                 repository
-                                                .findById(persona.getId())
+                                                .findById(personaCreada.getId())
                                                 .isPresent());
-                repository.deleteById(persona.getId());
+                repository.deleteById(personaCreada.getId());
         }
 
         @Test
         public void update() {
-            Persona persona = new Persona();
-                persona.setNombre("Paula");
-                persona.setApellido("Gomez");
-                persona.setProvincia_id(55l);
-                
-
-                repository.save(persona);
                 assertTrue(
                                 repository
-                                                .existsById(persona.getId()));
-                persona.setNombre("Lucia");
-                repository.save(persona);
-                Optional<Persona> updatedPersona= repository.findById(persona.getId());
+                                                .existsById(personaCreada.getId()));
+                personaCreada.setNombre("Lucia");
+                repository.save(personaCreada);
+                Optional<Persona> updatedPersona= repository.findById(personaCreada.getId());
                 assertTrue(updatedPersona.isPresent());
                 assertTrue(updatedPersona.get().getNombre() == "Lucia");
         }
